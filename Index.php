a46b4aead6898f272b07731df28d308ff148a6fc
Lekoorigan <trois14@live.fr>
2015-08-29 23:45:38 +0200

    Ajout de commentaire de guide dans le fichier index.php

---
+++ Index.php
@@ -0,0 +1,9 @@
+<? php
+
+// creation d'une session
+
+// si aucune session existante pour ce visiteur
+// alors demande la page d'accueil à controleur
+// sinon demande la page recherchée à controleur
+
+?>
